31955afb63fcfd2cfbb420c23cbc13acd20aee4b
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2024-03-01 12:56:31 +0100

    get post

--- src/index.js
+++ src/index.js
@@ -12,16 +12,44 @@ const port = 4000;
 
 
 async function getDB() {
-    const dataBase = await mysql.createConnection({
+    const conex = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'Diany3101',
-      database: 'dogs',
+      database: 'dogs_db',
     });
-    await dataBase.connect();
-    return dataBase;
+    conex.connect();
+    return conex;
 }
 
 server.listen(port, () => {
     console.log(`El servidor se esta ejecutando en el puerto ${port}`);
   });
+
+
+server.get('/dogs', async(req, res) => {
+    const conexDataBase = await getDB();
+
+    const dogsSQL = "select * from dogs";
+
+    const [result] = await conexDataBase.query(dogsSQL);
+
+    res.json({result});
+})
+
+server.post('/dogs', async(req, res) =>{
+
+    const data = req.body;
+
+    const {nombreMascota, razaMascota, edadMascota, sexoMascota, descripcionMascota} = data;
+
+    const conexDataBase = await getDB();
+
+    const sql  = "insert intro dogs (nameDog, raceDog, ageDog, genderDog, descriptionDog) values (?, ?, ?, ?, ?)";
+
+    const [result] = await conexDataBase.query(sql, [ nombreMascota, razaMascota, edadMascota, sexoMascota, descripcionMascota
+    ]);
+
+    res.json({result});
+
+})
